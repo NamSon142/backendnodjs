61363be4b46afd87d14ea317db4a944edea90cd0
Nam Son <sondua142@gmail.com>
2022-05-17 09:32:40 +0700

    first commit

---
+++ src/route/web.js
@@ -0,0 +1,13 @@
+import express from "express";
+import res from "express/lib/response";
+
+let router = express.Router();
+
+let initWebRoutes = (app) => {
+    router.get("/", (req, res0)=>{
+        return res.send("Hello Nam Son")
+    });
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
